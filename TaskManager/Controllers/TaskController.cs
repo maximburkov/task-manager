b6c56b94ae039a3d5d5f1c651e33c1333c9f3679
maximburkov <maxburkov12@gmail.com>
2020-01-13 16:59:45 +0300

    task stub model/controller added

---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Task = TaskManager.Models.Task;
+
+namespace TaskManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        #region Stubs
+        private static readonly Task[] tasks = new[]
+        {
+            new Task
+            {
+                Subject = "Development",
+                Description = "Do nice"
+            },
+            new Task
+            {
+                Subject = "Analytic",
+                Description = "Plan well. Very Urgent task!"
+            }
+        };
+        #endregion
+
+        private readonly ILogger<TaskController> _logger;
+
+        public TaskController(ILogger<TaskController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Task> Get()
+        {
+            return tasks;
+        }
+    }
+}
