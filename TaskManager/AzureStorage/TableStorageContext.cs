7a11481769da69c759c00ab6c8496977d04f6e6d
maximburkov <maxburkov12@gmail.com>
2020-01-29 18:52:19 +0300

    added summary to tstorage context

--- TaskManager/AzureStorage/TableStorageContext.cs
+++ TaskManager/AzureStorage/TableStorageContext.cs
@@ -20,6 +20,11 @@ namespace TaskManager.AzureStorage
             _tables = new ConcurrentDictionary<string, CloudTable>();
         }
 
+        /// <summary>
+        /// Get CloudTable object by table name
+        /// </summary>
+        /// <param name="tableName">Table name</param>
+        /// <returns></returns>
         private async Task<CloudTable> GetTableAsync(string tableName)
         {
             if (!_tables.TryGetValue(tableName, out var table))
@@ -32,6 +37,13 @@ namespace TaskManager.AzureStorage
             return table;
         }
 
+        /// <summary>
+        /// Retrieve element by row key and partition key. Fastest way to get element from table.
+        /// </summary>
+        /// <typeparam name="T">Element type</typeparam>
+        /// <param name="tableName">Table name</param>
+        /// <param name="rowKey">Row key</param>
+        /// <param name="partitionKey">Partition key</param>
         public async Task<T> GetAsync<T>(string tableName, string rowKey, string partitionKey) where T : class, ITableEntity
         {
             var table = await GetTableAsync(tableName);
@@ -40,16 +52,33 @@ namespace TaskManager.AzureStorage
             return queryResult.Result as T;
         }
 
+        /// <summary>
+        /// Get all elements from table
+        /// </summary>
+        /// <typeparam name="T">Element type</typeparam>
+        /// <param name="tableName">Table name</param>
         public async Task<IEnumerable<T>> GetAllAsync<T>(string tableName) where T : class, ITableEntity, new()
         {
             return await QueryAsync<T>(tableName, new TableQuery<T>());
         }
 
+        /// <summary>
+        /// Query elements by parameters
+        /// </summary>
+        /// <typeparam name="T">Element type</typeparam>
+        /// <param name="tableName">Table name</param>
+        /// <param name="tableQuery">Table query</param>
         public async Task<IEnumerable<T>> QueryWithParametersAsync<T> (string tableName, TableQuery<T> tableQuery) where T : class, ITableEntity, new()
         {
             return await QueryAsync<T>(tableName, tableQuery);
         }
 
+        /// <summary>
+        /// Add element in table
+        /// </summary>
+        /// <param name="tableName">Table name</param>
+        /// <param name="model">Element we want to add</param>
+        /// <returns></returns>
         public async Task<ITableEntity> AddAsync(string tableName, ITableEntity model)
         {
             var table = await GetTableAsync(tableName);
@@ -58,6 +87,11 @@ namespace TaskManager.AzureStorage
             return model;
         }
 
+        /// <summary>
+        /// Update element in table
+        /// </summary>
+        /// <param name="tableName">Table name</param>
+        /// <param name="model">Element we want to update</param>
         public async Task<ITableEntity> UpdateAsync(string tableName, ITableEntity model)
         {
             var table = await GetTableAsync(tableName);
@@ -76,6 +110,13 @@ namespace TaskManager.AzureStorage
             return model;
         }
 
+        /// <summary>
+        /// Delete element if exists
+        /// </summary>
+        /// <typeparam name="T">Element type</typeparam>
+        /// <param name="tableName">Table name</param>
+        /// <param name="rowKey">Row key</param>
+        /// <param name="partitionKey">Partition key</param>
         public async Task DeleteAsync<T>(string tableName, string rowKey, string partitionKey) where T : class, ITableEntity
         {
             var table = await GetTableAsync(tableName);
@@ -87,7 +128,6 @@ namespace TaskManager.AzureStorage
             var deleteOperation = TableOperation.Delete(entityToDelete);
             await table.ExecuteAsync(deleteOperation);
         }
-
 
         private async Task<IEnumerable<T>> QueryAsync<T>(string tableName, TableQuery<T> query) where T : class, ITableEntity, new()
         {
